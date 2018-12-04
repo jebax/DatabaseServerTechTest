eff03b2882ab0e01a79cdb32999f333721812da3
jebax <joel.eliot.baxter@gmail.com>
2018-12-04 16:31:49 +0000

    Renamed 'app' to 'server', 'index.js' to 'server.js'

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const url = require('url')
-const app = express()
+const server = express()
 const port = 4000
 const DataStorerClass = require('./data-storer.js')
 
 let dataStorer = new DataStorerClass()
 
-app.get('/set', (req, res) => {
+server.get('/set', (req, res) => {
   let params = url.parse(req.url, true).query
 
   dataStorer.set(params)
   res.send('Data stored successfully')
 })
 
-app.get('/get', (req, res) => {
+server.get('/get', (req, res) => {
   let params = url.parse(req.url, true).query
 
   res.send(dataStorer.get(params))
 })
 
-app.listen(port)
+server.listen(port)
