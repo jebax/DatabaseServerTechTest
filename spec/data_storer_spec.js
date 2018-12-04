5d0915e7c865001adaab9e73a6639545bcde3976
jebax <joel.eliot.baxter@gmail.com>
2018-12-04 15:44:22 +0000

    DataStorer can set a key/value pair
    Uses set() method to extract key and value from passed params, then set in storedData property.

--- spec/data_storer_spec.js
+++ spec/data_storer_spec.js
@@ -1,3 +1,17 @@
 describe('Data Storer', () => {
   const DataStorer = require('../src/data-storer.js')
+  let dataStorer
+  let params
+
+  beforeEach(() => {
+    dataStorer = new DataStorer()
+    params = { 'testKey': 'testValue' }
+  })
+
+  describe('A data storer', () => {
+    it('stores a key and its associated value', () => {
+      dataStorer.set(params)
+      expect(dataStorer.storedData).toEqual(params)
+    })
+  })
 })
--- src/data-storer.js
+++ src/data-storer.js
@@ -1,3 +1,14 @@
-class DataStorer {}
+class DataStorer {
+  constructor() {
+    this.storedData = {}
+  }
+
+  set(params) {
+    let key = Object.keys(params)[0]
+    let value = Object.values(params)[0]
+    
+    this.storedData[key] = value
+  }
+}
 
 module.exports = DataStorer
