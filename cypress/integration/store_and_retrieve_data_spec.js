2faf03d7a1b817c9882d56264e8a66061b219ae3
jebax <joel.eliot.baxter@gmail.com>
2018-12-04 14:29:39 +0000

    App stores and retrieves different data
    For a /set request, server parses url query and stores the key and value in an object. For a /get request, it parses the url query and retrieves the value of the specified key in the object.

--- cypress/integration/store_and_retrieve_data_spec.js
+++ cypress/integration/store_and_retrieve_data_spec.js
@@ -1,7 +1,16 @@
 describe('Storing and retrieving data', () => {
-  it('successfully stores data, retrieves it and sees it on screen', () => {
+  it('stores data, retrieves it and sends it as response body', () => {
     let testKey = 'testKey'
     let testValue = 'testValue'
+
+    cy.request(`http://localhost:3000/set?${testKey}=${testValue}`)
+    cy.request(`http://localhost:3000/get?key=${testKey}`)
+      .its('body').should('equal', testValue)
+  })
+
+  it('stores and retrieves different data', () => {
+    let testKey = 'testKey2'
+    let testValue = 'testValue2'
 
     cy.request(`http://localhost:3000/set?${testKey}=${testValue}`)
     cy.request(`http://localhost:3000/get?key=${testKey}`)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const url = require('url')
 const app = express()
 const port = 3000
 
+let storedData = {}
+
 app.get('/set', (req, res) => {
+  let request_query = url.parse(req.url, true).query
+  let key = Object.keys(request_query)[0]
+  let value = Object.values(request_query)[0]
+
+  storedData[key] = value
   res.send('')
 })
 
 app.get('/get', (req, res) => {
-  res.send('testValue')
+  let request_query = url.parse(req.url, true).query
+  let key = Object.values(request_query)[0]
+
+  res.send(storedData[key])
 })
 
 app.listen(port)
