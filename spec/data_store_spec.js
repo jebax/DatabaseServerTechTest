fa75812f817bc5a1866dc08103bac81a0b72a810
jebax <joel.eliot.baxter@gmail.com>
2018-12-05 10:58:27 +0000

    DataStore can write data to its storage
    Takes key value pair and writes them to property of data object

--- spec/data_store_spec.js
+++ spec/data_store_spec.js
@@ -1,3 +1,19 @@
 describe('Data Store', () => {
   const storeExports = require('../src/data-store.js')
+  let dataStore
+  let testKey
+  let testValue
+
+  beforeEach(() => {
+    dataStore = new storeExports.DataStore()
+    testKey = 'testKey'
+    testValue = 'testValue'
+  })
+
+  describe('A data store', () => {
+    it('writes a key and its associated value to its storage', () => {
+      dataStore.write(testKey, testValue)
+      expect(dataStore.data).toEqual({ testKey: 'testValue' })
+    })
+  })
 })
--- src/data-store.js
+++ src/data-store.js
@@ -1,3 +1,11 @@
-class DataStore {}
+class DataStore {
+  constructor() {
+    this.data = {}
+  }
+
+  write(key, value) {
+    this.data[key] = value
+  }
+}
 
 module.exports = { DataStore: DataStore }
